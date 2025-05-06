b3d42c8c84cd03b178cf8e294f23c2365d63ab0a
Vaibhav Singh <vaibhav.singh@sixt.com>
2025-05-07 01:35:52 +0530

    init

--- src/main/java/br/com/example/davidarchanjo/repository/AppRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.example.davidarchanjo.repository;
-
-import br.com.example.davidarchanjo.model.domain.App;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppRepository extends JpaRepository<App, Long> {
-}
